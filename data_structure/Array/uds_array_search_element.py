1211c5afce22cb3deb2dfa539c30226295487158
bwthadoop <hadoopbwt.batch21@gmail.com>
2026-02-28 11:49:07 +0530

    session 32: searching elements

--- data_structure/Array/uds_array_search_element.py
+++ data_structure/Array/uds_array_search_element.py
@@ -1,6 +1,12 @@
 from array import array
 
 if __name__ == '__main__':
+    # Here
+    # Time complexity : O(N)
+    # Space complexity: O(1)
+    # Point to remember
+    # range and len function is having time complexity as O(1)
+
     arr1 = array('i', [1, 2, 3, 4, 5, 6])
 
     def linear_search(array, target):
@@ -11,3 +17,4 @@ if __name__ == '__main__':
 
 
     print(linear_search(arr1, 5))
+    print(linear_search(arr1, 8))
